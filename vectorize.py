8f0bcb7d860ba9f9731ced63af01e06aedbf67f8
Shane Peelar <lookatyouhacker@gmail.com>
2017-04-02 16:40:38 -0400

    Model saving, loading, and similarity

--- vectorize.py
+++ vectorize.py
@@ -7,38 +7,58 @@ import random
 
 parser = argparse.ArgumentParser(description="Vectorize metadata files")
 
-parser.add_argument("metadata", metavar="file", type=str, nargs="+",
+parser.add_argument("--train", metavar="file", type=str, nargs="+",
         help="A metadata file");
+
+parser.add_argument("--model", metavar="model", type=str, help="Pre-trained doc2vec model (use --train to create)")
+
+parser.add_argument("-e", "--evaluate", action="store_true", help="Evaluate model (requires --train or --model)")
+
+parser.add_argument("--findsimilar", help="Find all documents similar to the listed one")
 
 args = parser.parse_args()
 
-files = args.metadata
+model_dm = ""
 
-tags = []
-for f in files:
-    print("Reading " + f)
-    for line in open(f):
-        components = line.split("\t")
-        tags.append(TaggedDocument(components[2].split(), [components[0]]))
+if args.model is not None:
+    print("Loading " + args.model)
+    model_dm = Doc2Vec.load(args.model)
+elif args.train is not None:
+    files = args.train
 
-#Randomize document order first
-print("Randomizing document order")
-random.shuffle(tags)
+    tags = []
+    for f in files:
+        print("Reading " + f)
+        for line in open(f):
+            components = line.split("\t")
+            tags.append(TaggedDocument(components[2].split(), [components[0]]))
 
-print("Building model")
-model_dm = Doc2Vec(documents=tags, alpha=0.025, size=100, workers=4, dbow_words=1, iter=100, min_count=2)
-
-for i in range(10):
-    print("Pass {}".format(i))
+    #Randomize document order first
+    print("Randomizing document order")
     random.shuffle(tags)
-    model_dm.train(tags)
-    model_dm.alpha -= 0.002
-    model_dm.min_alpha = model_dm.alpha
 
-print("Writing word2vec format: wordvectors.txt")
-model_dm.wv.save_word2vec_format("wordvectors.txt")
+    print("Building model")
+    model_dm = Doc2Vec(documents=tags, alpha=0.025, size=100, workers=4, dbow_words=1, iter=100, min_count=2)
 
-print("Writing doc2vec format: docvectors.txt")
-model_dm.save("docvectors.txt")
+    for i in range(10):
+        print("Pass {}".format(i))
+        random.shuffle(tags)
+        model_dm.train(tags)
+        model_dm.alpha -= 0.002
+        model_dm.min_alpha = model_dm.alpha
 
-print("Done")
+    print("Writing word2vec format: wordvectors.txt")
+    model_dm.wv.save_word2vec_format("wordvectors.txt")
+
+    print("Writing doc2vec format: docvectors.model")
+    model_dm.save("docvectors.model")
+
+    print("Done")
+
+if args.evaluate and model_dm != "":
+    print("Evaluating model")
+
+if args.findsimilar and model_dm != "":
+    print("Documents similar to: " + args.findsimilar)
+    simdocs = model_dm.docvecs.most_similar(["7EAE63BC"])
+    print(simdocs)
